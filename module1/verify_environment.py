d172afd27b2d7ae336ca772307cbc7ffb6d5aa30
Julie Y <yeomanj7782@student.faytechcc.edu>
2026-01-25 03:41:09 +0000

    vereified environment

---
+++ module1/verify_environment.py
@@ -0,0 +1,7 @@
+import platform
+import sys
+
+print("Environment validation is successful")
+print("Name: Julie Yeoman")
+print("Python version:", sys.version)
+print("Platform:", platform.system())
